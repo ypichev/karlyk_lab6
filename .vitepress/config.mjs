d6c9c2718c2cdd6e63597379559cdee02919fbea
varshavskiy_png <timikinvarshav05@gmail.com>
2025-03-10 20:55:01 +0200

    Try to fix 1

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "FlexForm",
   description: "none",
-  base: '/repo/',
+  base: '/base/',
   outDir: '.vitepress/dist',
 
   themeConfig: {
