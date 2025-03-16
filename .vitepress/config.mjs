15857debaccaf63e65b466a864f090884f4b2613
Tymur Varshavskiy <74325939+varshavskiy05@users.noreply.github.com>
2025-03-16 22:32:20 +0200

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "FlexForm",
   description: "none",
-  base: '/db_lab1/',
+  base: '/db_lab/',
 
   themeConfig: {
     nav: [
