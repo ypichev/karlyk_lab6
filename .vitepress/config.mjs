9cf2f44be8079f73d337101a9dcac8ec4d338078
Tymur Varshavskiy <74325939+varshavskiy05@users.noreply.github.com>
2025-05-09 12:35:06 +0300

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -24,7 +24,7 @@ export default defineConfig({
       { text: 'UseCase', link: '/useCase'},
       { text: 'Бізнес модель та діаграма системи опитування', link: '/business_models'},
       { text: 'Реалізація інформаційного та програмного забезпечення', link: '/SQL' },
-      {text: 'Реалізація об’єктно-реляційного відображення', link: '/Implementation'}, 
+      { text: 'Реалізація об’єктно-реляційного відображення', link: '/Implementation'}, 
       { text: 'Тестування працездатності системи', link: '/Test' },
       { text: 'Висновки', link: '/vusnovki' },
       { text: 'Автори', link: '/autors' },
